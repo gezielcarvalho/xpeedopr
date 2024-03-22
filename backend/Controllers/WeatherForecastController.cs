53c836332b8b1fb10636f03827d04d7107fb618c
Geziel Carvalho <geziel.natal@gmail.com>
2024-03-22 12:12:40 -0400

    Reset backend #2

--- backend/Controllers/WeatherForecastController.cs
+++ backend/Controllers/WeatherForecastController.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers
@@ -19,7 +18,7 @@ namespace backend.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast"), Authorize]
+        [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,8 +1,3 @@
-using backend.Data;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.Filters;
 
 namespace backend
 {
@@ -13,35 +8,11 @@ namespace backend
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc("v1", new() {Title = "backend", Version = "v1"});
-                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-                {
-                    In = ParameterLocation.Header,
-                    Description = "Please insert JWT with Bearer into field",
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey
-                });
-                
-                options.OperationFilter<SecurityRequirementsOperationFilter>();
-            });
-            
-            builder.Services.AddDbContext<DataContext>(options =>
-            {
-                // Configure the context to use Microsoft SQL Server.
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-            });
-            
-            // Add services to the container.
-            builder.Services.AddAuthorization();
-            
-            // Add services to the container.
-            builder.Services.AddIdentityApiEndpoints<IdentityUser>()
-                .AddEntityFrameworkStores<DataContext>();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
@@ -52,10 +23,13 @@ namespace backend
                 app.UseSwaggerUI();
             }
 
-            app.MapIdentityApi<IdentityUser>();
             app.UseHttpsRedirection();
+
             app.UseAuthorization();
+
+
             app.MapControllers();
+
             app.Run();
         }
     }
