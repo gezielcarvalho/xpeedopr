41b7a5ffc40b86cddd4e7947ff7b84fe251a75ff
Geziel Carvalho <geziel.natal@gmail.com>
2024-03-22 12:25:07 -0400

    Clean up code. #2

--- backend/WeatherForecast.cs
+++ backend/WeatherForecast.cs
@@ -4,9 +4,7 @@ namespace backend
     {
         public DateOnly Date { get; set; }
 
-        public int TemperatureC { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureC { get; init; }
 
         public string? Summary { get; set; }
     }
